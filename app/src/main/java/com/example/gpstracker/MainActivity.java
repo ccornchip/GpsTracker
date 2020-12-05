ef8f7a2ee5a5041bf0a96ab1dc297e0d768da93d
Christophe Cornet <ccornchip@hotmail.com>
2020-12-05 14:24:52 +0100

    Add button

--- app/src/main/java/com/example/gpstracker/MainActivity.java
+++ app/src/main/java/com/example/gpstracker/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.gpstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private boolean serviceIsStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button);
+        serviceIsStarted = false;
+        updateButtonText();
+        mButton.setOnClickListener(v -> {
+            serviceIsStarted = !serviceIsStarted;
+            updateButtonText();
+        });
+    }
+
+    private void updateButtonText() {
+        if (serviceIsStarted) {
+            mButton.setText(R.string.start_text);
+        } else {
+            mButton.setText(R.string.stop_text);
+        }
     }
 }
