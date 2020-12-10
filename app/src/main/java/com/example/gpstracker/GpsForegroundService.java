b541331f836d48657ab2450f1851bf23f40a105b
cornetc <christophe.cornet@ifp.fr>
2020-12-10 20:14:37 +0100

    Add GPS and web socket server

--- app/src/main/java/com/example/gpstracker/GpsForegroundService.java
+++ app/src/main/java/com/example/gpstracker/GpsForegroundService.java
@@ -1,20 +1,39 @@
 package com.example.gpstracker;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
 import android.os.IBinder;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.function.Consumer;
 
 public class GpsForegroundService extends Service {
     public static final String CHANNEL_ID = "CHANNEL_ID";
 
+    private LocationManager mLocationManager;
+    private MyWebSocketServer wss;
+
     private boolean isRunning = false;
     private Consumer<Boolean> isRunningListener;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
 
     public class GpsBinder extends Binder {
         public void setIsRunningListener(Consumer<Boolean> isRunningListener) {
@@ -31,17 +50,30 @@ public class GpsForegroundService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
+
+        // Prepare foreground service
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle("Energyk")
                 .setContentText("We're watching you")
                 .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
         startForeground(1, builder.build());
+
+        // Start the web socket
+        int port = 3000;
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+        wss = new MyWebSocketServer(inetSocketAddress);
+        wss.start();
+
+        // Set the GPS listener
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+        // Notify bound applications that the service is running
         isRunning = true;
         if (isRunningListener != null) isRunningListener.accept(true);
 
@@ -51,7 +83,43 @@ public class GpsForegroundService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        // Stop the web socket
+        try {
+            wss.stop();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+
+        // Unset the GPS listener
+        mLocationManager.removeUpdates(locationListener);
+
+        // Notify bound applications that the service has stopped
         isRunning = false;
         if (isRunningListener != null) isRunningListener.accept(false);
     }
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+            Toast.makeText(GpsForegroundService.this, "GPS disabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+        }
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            long time = location.getTime();
+            String json = "{\"timestamp\":" + time +
+                    ",\"latitude\":" + latitude +
+                    ",\"longitude\":" + longitude + "}";
+            wss.sendMessage(json);
+            System.out.println(json);
+        }
+    };
+
 }
