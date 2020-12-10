17049e842aa2eeae570c93eb2581b03afbc9e744
cornetc <christophe.cornet@ifp.fr>
2020-12-10 19:18:39 +0100

    nettoyage

--- app/src/main/java/com/example/gpstracker/MainActivity.java
+++ app/src/main/java/com/example/gpstracker/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.gpstracker;
 
 import android.Manifest;
-import android.app.ActivityManager;
-import android.app.Application;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -22,10 +20,8 @@ import java.net.SocketException;
 import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity implements ServiceConnection {
-    private static final String SERVICE_RUNNING = "SERVICE_RUNNING";
 
     private Button mButton;
-    private boolean serviceIsStarted = false;
     private Intent gpsForegroundServiceIntent;
 
     @Override
@@ -34,15 +30,10 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         gpsForegroundServiceIntent = new Intent(this, GpsForegroundService.class);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null) {
-            serviceIsStarted = savedInstanceState.getBoolean(SERVICE_RUNNING);
-        }
-
         TextView textView = findViewById(R.id.textView);
         textView.setText(getIpAddress());
 
         mButton = findViewById(R.id.button);
-//        updateButtonText();
         mButton.setOnClickListener(v -> {
             textView.setText(getIpAddress());
 
@@ -51,12 +42,8 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
             } else {
                 stopService(gpsForegroundServiceIntent);
             }
-//            serviceIsStarted = !serviceIsStarted;
-//            updateButtonText();
         });
-
         bindService(gpsForegroundServiceIntent, this, BIND_ABOVE_CLIENT);
-
     }
 
     @Override
@@ -65,39 +52,14 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         askPermissionsAndStartService();
     }
 
-    private boolean isMyServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     private void askPermissionsAndStartService() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
-//            startService(new Intent(this, MyGpsService.class));
             startForegroundService(gpsForegroundServiceIntent);
             bindService(gpsForegroundServiceIntent, this, BIND_ABOVE_CLIENT);
         }
     }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(SERVICE_RUNNING, serviceIsStarted);
-    }
-
-//    private void updateButtonText() {
-//        if (!serviceIsStarted) {
-//            mButton.setText(R.string.start_text);
-//        } else {
-//            mButton.setText(R.string.stop_text);
-//        }
-//    }
 
     public static String getIpAddress() {
         try {
@@ -120,27 +82,14 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder binder) {
-        System.out.println("connected");
         GpsForegroundService service = ((GpsForegroundService.GpsBinder) binder).getService();
 
-        service.setIsRunningListener(isRunnning -> {
-            if (isRunnning) {
-                mButton.setText(R.string.stop_text);
-            } else {
-                mButton.setText(R.string.start_text);
-            }
-        });
-
-        System.out.println();
+        service.setIsRunningListener(isRunning ->
+                mButton.setText(isRunning ? R.string.stop_text : R.string.start_text)
+        );
     }
 
     @Override
     public void onServiceDisconnected(ComponentName name) {
-        System.out.println("disconnected");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 }
